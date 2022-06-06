634e3720b697f5bc376b667090d07996402ef9b8
Parkseonho <tjsgh2103@gmail.com>
2022-06-06 19:45:02 +0900

    레이아웃 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Layout from "../components/Layout";
 
 const IndexPage = () => {
   return (
-    <main>
+    <Layout pageTitle="HomePage">
       <p>hi</p>
-    </main>
+    </Layout>
   );
 };
 
