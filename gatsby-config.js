ca3d7ef749a6429a4981da59111cbe03d9fe3c5d
Parkseonho <tjsgh2103@gmail.com>
2022-06-06 19:56:12 +0900

    타이틀 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    siteMetadata: {
+      title: "My First Gatsby Site",
+    },
   },
   plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
 };
